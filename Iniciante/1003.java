996e6c83f231d85e4b073bcae54f1e6beb7de0fb
livia-oliveira <livia0liveiraslv93@gmail.com>
2023-05-18 14:33:42 -0300

    Questão 1003

---
+++ Iniciante/1003.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner entrada = new Scanner(System.in);
+
+        int numero1, numero2, soma;
+
+        numero1 = entrada.nextInt();
+        numero2 = entrada.nextInt();
+
+        soma = numero1+numero2;
+
+        System.out.println("SOMA = "+soma);
+ 
+    }
+ 
+}
